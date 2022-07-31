32d2138cde4279848ba1cac83c59f69b0645461b
Elina <elina.melkonyan.y@tumo.org>
2022-07-31 18:37:34 +0400

    THIRD

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -2,5 +2,9 @@ package lang.print.gaps.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a=4;
+        int b =2;
+        double c = (Math.pow(a,2)-5*b +2+a+7)*((a+b-4*b*a)/2);
+        System.out.println(c);
     }
 }
--- src/main/java/lang/print/gaps/task4/IntRepresentation.java
+++ src/main/java/lang/print/gaps/task4/IntRepresentation.java
@@ -2,5 +2,17 @@ package lang.print.gaps.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;
+        char b = (char)c;
+        System.out.println(b);
+        int h = 104;
+        char k = (char) h;
+        System.out.println(k);
+        int a = 65;
+        char t = (char)a;
+        System.out.println(t);
+        int r= 114;
+        char g = (char)r;
+        System.out.println(g);
     }
 }
